59d7d70eeb2a0b7f85d5b36498f8bd2cd6a808ed
AshanFernando <ashan256@gmail.com>
2015-02-08 22:13:25 +0530

    Moving libs to external folder, use qunit from external, combine unit test file to all.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,90 +1,96 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.initConfig({
+    grunt.initConfig({
 
-    pkg: grunt.file.readJSON('package.json'),
-	
-	bower: grunt.file.readJSON('./.bowerrc'),
+        pkg: grunt.file.readJSON('package.json'),
 
-    concat: {
-      options: {
-        separator: "\n\n"
-      },
-      dist: {
-        src: [
-          'src/_intro.js',
-          'src/main.js',
-          'src/_outro.js'
-        ],
-        dest: 'dist/<%= pkg.name.replace(".js", "") %>.js'
-      }
-    },
-	
-	copy: {
-	  dest: 'dist/libs',
-      dist: {
-       files: [{
-         expand: true,
-         cwd: '<%= bower.directory %>/jquery/dist',
-         src: 'jquery.min.js',
-         dest: '<%= copy.dest %>'
-       },
-       {
-         expand: true,
-         cwd: '<%= bower.directory %>/underscore',
-         src: 'underscore-min.js',
-         dest: '<%= copy.dest %>',
-		 rename: function(dest, src) {
-			return dest + '/' + src.replace('-','.');
-		}
-       }
-	   ]
-      }
-    },
-	
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name.replace(".js", "") %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'dist/<%= pkg.name.replace(".js", "") %>.min.js': ['<%= concat.dist.dest %>'],
-		  '<%= copy.dest %>/backbone.all.min.js': [ '<%= bower.directory %>/backbone/backbone.js',  '<%= bower.directory %>/backbone-relational/backbone-relational.js' ],
-        }
-      }
-    },
+        bower: grunt.file.readJSON('./.bowerrc'),
 
-    qunit: {
-      files: ['test/*.html']
-    },
-
-    jshint: {
-      files: ['dist/backstorage.js'],
-      options: {
-        globals: {
-          console: true,
-          module: true,
-          document: true
+        concat: {
+            options: {
+                separator: "\n\n"
+            },
+            basic: {
+                src: [
+                  'src/_intro.js',
+                  'src/main.js',
+                  'src/_outro.js'
+                ],
+                dest: 'dist/<%= pkg.name.replace(".js", "") %>.js'
+            },
+            extras : {
+                src: [
+                   'test/units/*.js'
+                ],
+                dest: 'test/all.js',
+            }
         },
-        jshintrc: '.jshintrc'
-      }
-    },
 
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['concat', 'jshint', 'qunit']
-    }
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= bower.directory %>/jquery/dist',
+                    src: 'jquery.min.js',
+                    dest: 'dist/libs'
+                },
+                {
+                    expand: true,
+                    cwd: '<%= bower.directory %>/underscore',
+                    src: 'underscore-min.js',
+                    dest: 'dist/libs',
+                    rename: function (dest, src) {
+                        return dest + '/' + src.replace('-', '.');
+                    }
+                }
+                ]
+            }
+        },
 
-  });
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name.replace(".js", "") %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name.replace(".js", "") %>.min.js': ['<%= concat.basic.dest %>'],
+                    'dist/libs/backbone.min.js': ['<%= bower.directory %>/backbone/backbone.js'],
+                    'dist/libs/backbone-relational.min.js': ['<%= bower.directory %>/backbone-relational/backbone-relational.js']
+                }
+            }
+        },
 
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+        qunit: {
+            files: ['test/*.html']
+        },
 
-  grunt.registerTask('test', ['jshint', 'qunit']);
-  grunt.registerTask('default', ['copy', 'concat', 'jshint', 'qunit', 'uglify']);
+        jshint: {
+            files: ['dist/backstorage.js'],
+            options: {
+                globals: {
+                    console: true,
+                    module: true,
+                    document: true
+                },
+                jshintrc: '.jshintrc'
+            }
+        },
+
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['concat', 'jshint', 'qunit']
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('test', ['jshint', 'qunit']);
+    grunt.registerTask('default', ['copy', 'concat', 'jshint', 'qunit', 'uglify']);
 
 };
